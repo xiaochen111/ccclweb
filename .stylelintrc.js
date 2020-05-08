0a127d8bd9cf3d6bfbb9d2a0a9b7d59952d72ae8
zhangaolin <zhangaolin@live.com>
2020-05-08 16:46:55 +0800

    lint

--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-const fabric = require('@umijs/fabric');
-
-module.exports = {
-  ...fabric.stylelint,
-  rules: {
-    // at-rule-no-unknown: 屏蔽一些scss等语法检查
-    'at-rule-no-unknown': [true, { ignoreAtRules: ['mixin', 'extend', 'content'] }],
-  },
-};
